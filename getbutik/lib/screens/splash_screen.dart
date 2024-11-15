baa3ae18adddfd10deef678a78a2f834b44bc8c5
nadula14 <madugalle_ewmwwndb_e22@engug.ruh.ac.lk>
2024-11-15 20:13:38 +0530

    splash

---
+++ getbutik/lib/screens/splash_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:getbutik/screens/main_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MainScreen ()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                "assets/logo.png",
+                width: MediaQuery.of(context).size.height * 0.3,
+                height: MediaQuery.of(context).size.height * 0.3,
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+            Center(
+              child: Image.asset(
+                "assets/brand.jpeg",
+                width: MediaQuery.of(context).size.height * 0.3,
+                height: MediaQuery.of(context).size.height * 0.2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
